47b16de7360c4ce165ca536e786f329e33044206
Edmund von der Burg <evdb@mysociety.org>
2013-11-14 21:25:16 +0000

    Actually fetch and display the prices

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var program = require('commander'),
     isbn    = require('./src/isbn'),
+    async   = require('async'),
+    _       = require('underscore'),
     request = require('superagent');
 
 var apiBase = 'http://api.127.0.0.1.xip.io:3000/v1/';
+// var apiBase = 'http://api.openbookprices.com/v1/';
 
 program
   .version('0.0.1')
@@ -39,6 +42,52 @@ function handleInitialRequest (res) {
     res.body.request.country
   );
 
-  console.log(res.body);
+  // console.log(res.body);
 
+  async.each(
+    res.body.results,
+    handleVendorRequest,
+    allDone
+  );
+
+}
+
+function handleVendorRequest (data, cb) {
+
+  // console.log(data);
+
+  if (data.meta.retryDelay) {
+    // console.log("Retrying in %s", data.meta.retryDelay);
+    setTimeout(
+      function () {
+        // console.log("GET %s", data.request.url);
+
+        request
+          .get(data.request.url)
+          .on('error', function (err) {
+            console.warn(err);
+            cb();
+          })
+          .end(function (res) {
+            handleVendorRequest(res.body, cb);
+          });
+      },
+      data.meta.retryDelay * 1000
+    );
+  } else {
+    _.each(data.offers, function (offer, condition) {
+      console.log(
+        "%s (%s) from %s: %s",
+        offer.total,
+        condition,
+        data.vendor.name,
+        offer.url || data.vendor.url
+      );
+    });
+    cb();
+  }
+}
+
+function allDone (err) {
+  console.log("all done!");
 }
