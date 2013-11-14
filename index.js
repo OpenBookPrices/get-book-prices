14983cd0254538326ce4f38f9014fa55bd51fada
Edmund von der Burg <evdb@mysociety.org>
2013-11-14 20:50:03 +0000

    Basic fetching of initial results, showing country and currency selected

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var program = require('commander');
+var program = require('commander'),
+    isbn    = require('./src/isbn'),
+    request = require('superagent');
+
+var apiBase = 'http://api.127.0.0.1.xip.io:3000/v1/';
 
 program
   .version('0.0.1')
@@ -7,4 +11,34 @@ program
 
 if (program.args.length == 0) {
   program.help();
+}
+
+var isbn = isbn.clean(program.args[0]);
+if (!isbn ) {
+  console.error("The ISBN provided is not valid: '%s'", program.args[0]);
+  process.exit(1);
+}
+
+var initialURL = apiBase + "books/" + isbn + "/prices";
+
+request
+  .get(initialURL)
+  .on("error", function (err) {
+    console.error(err);
+    process.exit();
+  })
+  .end(handleInitialRequest);
+
+
+function handleInitialRequest (res) {
+
+  console.log(
+    "Fetching prices for '%s' in '%s', shipping to '%s'",
+    isbn,
+    res.body.request.currency,
+    res.body.request.country
+  );
+
+  console.log(res.body);
+
 }
